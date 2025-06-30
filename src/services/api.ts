0f348c3a8527d173285d1933a258c4b91979fc12
Leandro Farias <leandrorf1606@gmail.com>
2025-06-30 17:45:18 -0300

    try again

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,5 @@
-export const apiPublic = "https://mais-prati-leetcode.onrender.com/auth";
-export const apiPrivate = "https://mais-prati-leetcode.onrender.com";
+export const apiPublic = "https://mais-prati-leetcode.onrender.com";
+export const apiPrivate = "https://mais-prati-leetcode.onrender.com/home";
 
-// export const apiPublic = "http://localhost:3333/auth";
-// export const apiPrivate = "http://localhost:3333";
+// export const apiPublic = "http://localhost:3333";
+// export const apiPrivate = "http://localhost:3333/home";
