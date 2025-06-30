7944b5f4780200787f0e52001bc9a82f3fe8f8b1
Leandro Farias <leandrorf1606@gmail.com>
2025-06-30 15:26:45 -0300

    change path api

--- src/services/user-and-auth.ts
+++ src/services/user-and-auth.ts
@@ -1,49 +1,49 @@
-import { LoginSchema, RegisterSchema } from "@/app/(auth)/_validators";
+  import { LoginSchema, RegisterSchema } from "@/app/(auth)/_validators";
 
-export const api = "https://mais-prati-leetcode.onrender.com";
+  export const api = "https://mais-prati-leetcode.onrender.com/api";
 
-export async function createAccount(data: RegisterSchema) {
-  const res = await fetch(`${api}/register`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    credentials: "include", // Isso aqui faz enviar cookies
-    body: JSON.stringify(data),
-  });
+  export async function createAccount(data: RegisterSchema) {
+    const res = await fetch(`${api}/register`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      credentials: "include", // Isso aqui faz enviar cookies
+      body: JSON.stringify(data),
+    });
 
-  if (!res.ok) {
-    const error = await res.json();
-    throw new Error(error.message);
+    if (!res.ok) {
+      const error = await res.json();
+      throw new Error(error.message);
+    }
+
+    return res.json();
   }
 
-  return res.json();
-}
+  export async function singIn(data: LoginSchema) {
+    const res = await fetch(`${api}/login`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      credentials: "include", // Isso aqui faz enviar cookies
+      body: JSON.stringify(data),
+    });
+    return res.json();
+  }
 
-export async function singIn(data: LoginSchema) {
-  const res = await fetch(`${api}/login`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    credentials: "include", // Isso aqui faz enviar cookies
-    body: JSON.stringify(data),
-  });
-  return res.json();
-}
+  export async function logout() {
+    await fetch(`${api}/logout`, {
+      method: "POST",
+      credentials: "include",
+    });
+    return;
+  }
 
-export async function logout() {
-  await fetch(`${api}/logout`, {
-    method: "POST",
-    credentials: "include",
-  });
-  return;
-}
-
-export async function getUserInfo() {
-  const res = await fetch(`${api}/user`, {
-    method: "GET",
-    credentials: "include",
-  })
-  return res.json()
-}
+  export async function getUserInfo() {
+    const res = await fetch(`${api}/user`, {
+      method: "GET",
+      credentials: "include",
+    })
+    return res.json()
+  }
