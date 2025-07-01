6d8f2dac33440f1580dafda49e4680e249cff597
Leandro Farias <leandrorf1606@gmail.com>
2025-07-01 11:28:34 -0300

    test home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,13 @@
-import { ListsProvider } from "@/context/listfilter";
-import { AsideLists } from "./components/aside-lists";
-import { Header } from "./components/header";
-import { MainContent } from "./components/main-content";
-import { UserProvider } from "@/context/userInfo";
+// import { ListsProvider } from "@/context/listfilter";
+// import { AsideLists } from "./components/aside-lists";
+// import { Header } from "./components/header";
+// import { MainContent } from "./components/main-content";
+// import { UserProvider } from "@/context/userInfo";
 
 export default function Home() {
   return (
-    <ListsProvider>
-      <UserProvider>
-        <Header />
-        <div className="grid grid-cols-[380px_1fr]">
-          <AsideLists />
-          <MainContent />
-        </div>
-      </UserProvider>
-    </ListsProvider>
+    <div className="text-white text-3xl">
+      Home segura - acesso liberado!
+    </div>
   );
 }
