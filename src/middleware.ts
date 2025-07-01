efe29772fad6d28e3049d3e7dcf75d3827542064
Leandro Farias <leandrorf1606@gmail.com>
2025-07-01 13:10:23 -0300

    test without middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,10 +5,11 @@ export const config = {
 }
 
 export function middleware(req: NextRequest) {
-  const token = req.cookies.get("accessToken")?.value
+  console.log(req);
+  // const token = req.cookies.get("accessToken")?.value
 
-  if (!token) {
-    return NextResponse.redirect(new URL("/login", req.url))
-  }
+  // if (!token) {
+  //   return NextResponse.redirect(new URL("/login", req.url))
+  // }
   return NextResponse.next()
 }
