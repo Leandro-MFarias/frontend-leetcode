e044570f583e39ba485201ee2cca68e51a3b8716
Leandro Farias <leandrorf1606@gmail.com>
2025-06-30 23:13:01 -0300

    feat: railway domain

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,5 @@
-export const apiPublic = "https://mais-prati-leetcode.onrender.com";
-export const apiPrivate = "https://mais-prati-leetcode.onrender.com/home";
+export const apiPublic = "https://backend-leetcode-production.up.railway.app";
+export const apiPrivate = "https://backend-leetcode-production.up.railway.app/home";
 
 // export const apiPublic = "http://localhost:3333";
 // export const apiPrivate = "http://localhost:3333/home";
